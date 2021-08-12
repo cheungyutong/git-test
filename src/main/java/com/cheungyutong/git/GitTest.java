a0dedf4a44c4cf071c635528dbffe098ef672fa2
cheungyutong <957088679@qq.com>
2021-08-12 22:31:41 +0800

    first commit

---
+++ src/main/java/com/cheungyutong/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.cheungyutong.git;
+
+/**
+ * @Author cheungyutong
+ * @Date 2021/8/12 22:25
+ * @Deprecation
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+
+    }
+}
