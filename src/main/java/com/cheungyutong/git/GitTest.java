5ba730a9890df935370add7c18da96b7d2f59158
cheungyutong <66907123+cheungyutong@users.noreply.github.com>
2021-08-14 16:24:17 +0800

    Update GitTest.java
    commit from github

--- src/main/java/com/cheungyutong/git/GitTest.java
+++ src/main/java/com/cheungyutong/git/GitTest.java
@@ -3,7 +3,7 @@ package com.cheungyutong.git;
 /**
  * @Author cheungyutong
  * @Date 2021/8/12 22:25
- * @Deprecation
+ * @Deprecation Git学习
  */
 public class GitTest {
     public static void main(String[] args) {
